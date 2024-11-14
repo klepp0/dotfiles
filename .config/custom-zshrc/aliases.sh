ba28f58213c96a581452ec788d75251f07182cd8
klepp0 <jonas.kleppich@sap.com>
2024-11-14 09:29:04 +0100

    refactor: set verbose flag

--- .config/custom-zshrc/aliases.sh
+++ .config/custom-zshrc/aliases.sh
@@ -3,8 +3,8 @@ alias vim="nvim"
 alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
 
 # Delete local merged branches containing "klepp0"
-alias cleanup-branches='git branch --merged | grep "klepp0" | egrep -v "(^\*|main|master)" | xargs git branch -d'
+alias cleanup-branches='git branch --merged | grep "klepp0" | egrep -v "(^\*|main|master)" | xargs git branch -dv'
 
 # Delete remote merged branches containing "klepp0"
-alias prune-remote-branches='git branch -r --merged | grep "klepp0" | grep -vE "(^\*|master|main|HEAD)" | sed "s/origin\///" | xargs -n 1 git push --delete origin'
+alias prune-remote-branches='git branch -r --merged | grep "klepp0" | grep -vE "(^\*|master|main|HEAD)" | sed "s/origin\///" | xargs -n 1 git push --delete origin --verbose'
 
